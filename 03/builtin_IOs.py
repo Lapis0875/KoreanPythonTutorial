83ea1e43bb5a67b84ee06e793284c58b94c21461
Lapis0875 <lapis0875@gmail.com>
2020-08-03 03:11:39 +0900

    Finish writing Lecture 02 : Variable and Basic types & Lecture 03 : Built-in IO functions. Add README.mds in each lecture

---
+++ 03/builtin_IOs.py
@@ -0,0 +1,8 @@
+# 기본 출력함수 print입니다. print의 파라미터로 전달된 데이터를 콘솔에 출력합니다.
+print("Hello World!")
+print(100)
+print(True)
+print(3.14)
+
+# 기본 입력함수 input입니다. input의 파라미터로는 콘솔에 출력할 문자열을 전달하며, input 함수는 콘솔을 통해 입력받은 데이터를 반환합니다.
+answer = input("정답을 입력해주세요! : ")
