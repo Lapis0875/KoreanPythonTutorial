56a8ecf9173623a85487e36ee254804537b7a733
Lapis0875 <lapis0875@gmail.com>
2020-08-07 01:13:02 +0900

    Working on Lectures

---
+++ 03 - 입출력 함수/builtin_IOs.py
@@ -0,0 +1,11 @@
+# type 함수를 사용해, 변수가 담고 있는 데이터 혹은 직접 입력한 데이터의 자료형을 알 수 있어요.
+print(type(100))
+print(type(True))
+print(type("Hello World!"))
+print(type(3.14))
+
+"""
+# 기본 입력함수 input입니다. input의 파라미터로는 콘솔에 출력할 문자열을 전달하며, input 함수는 콘솔을 통해 입력받은 데이터를 반환합니다.
+answer = input("정답을 입력해주세요! : ")
+print("입력하신 정답은", answer, "입니다.")
+"""
---
+++ 04 - 사칙 연산자/operators.py
@@ -0,0 +1,19 @@
+a = 10 + 50
+print(a)
+
+b = 30 - 20
+print(b)
+
+c = 4 * 5
+print(c)
+
+d = 50 / 10
+print(d)
+
+e = 13 // 2
+print(e)
+f = 13 % 2
+print(f)
+
+a = (10 + 20) * 3
+print(a)
